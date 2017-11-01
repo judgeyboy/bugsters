30de0efda3fc5a00fc992a540841f0cd4d95491a
Ben Weber <bweber89@gmail.com>
2017-11-01 09:27:12 +0100

    Styles the contact form a bit

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,12 +1,13 @@
 <template>
-  <form @submit.prevent="onSubmit">
-    <legend>Contact</legend>
-    <input type="text" placeholder="name" v-model="name"> <br />
-    <input type="email" placeholder="email" v-model="email"> <br />
-    <input type="text" placeholder="subject" v-model="subject"> <br />
-    <textarea placeholder="message" v-model="message"></textarea> <br />
-    <input type="submit" value="Send">
-  </form>
+  <div class="contact-form">
+    <form @submit.prevent="onSubmit">
+      <input type="text" placeholder="name" v-model="name"> <br />
+      <input type="email" placeholder="email" v-model="email"> <br />
+      <input type="text" placeholder="subject" v-model="subject"> <br />
+      <textarea placeholder="message" v-model="message"></textarea> <br />
+      <input type="submit" value="Send" class="button">
+    </form>
+  </div>
 </template>
 
 <script>
@@ -38,3 +39,22 @@ export default {
 
 }
 </script>
+
+<style lang="sass" scoped>
+.contact-form
+  max-width: 400px
+  margin: 40px auto
+
+input[type=text], input[type=email], textarea
+  border-radius: 4px
+  width: 100%
+  font-size: 20px
+  line-height: 40px
+  margin-bottom: 20px
+
+input[type=text], input[type=email]
+  height: 40px
+
+textarea
+  height: 120px
+</style>

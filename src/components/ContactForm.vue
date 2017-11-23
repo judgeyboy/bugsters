7544bf65480d958beb14faa11f29a2ea9836fe54
Ben Weber <bweber89@gmail.com>
2017-11-23 20:22:07 +0100

    Styles the contact form with spectre's form styles

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,32 +1,71 @@
 <template>
   <div>
-    <form @submit.prevent="handleSubmit('contact-form')" data-vv-scope="contact-form">
-      <input type="text"
-             name="name"
-             placeholder="name"
-             v-model="name"
-             v-validate="'required'"
-             :class="{'is-danger': errors.has('contact-form.name')}"> <br />
+    <form class="form-horizontal"
+          @submit.prevent="handleSubmit('contact-form')"
+          data-vv-scope="contact-form">
 
-      <input type="email"
-             name="email"
-             placeholder="email"
-             v-model="email"
-             v-validate="'required|email'"
-             :class="{'is-danger': errors.has('contact-form.email')}"> <br />
+      <div class="form-group">
+        <div class="col-3">
+          <label class="form-label" for="name">Name</label>
+        </div>
+        <div class="col-9">
+          <input type="text"
+                 id="name"
+                 class="form-input"
+                 name="name"
+                 placeholder="name"
+                 v-model="name"
+                 v-validate="'required'"
+                 :class="{'is-error': errors.has('contact-form.name')}">
+        </div>
+      </div>
 
-      <input type="text"
-             name="subject"
-             placeholder="subject"
-             v-model="subject"
-             v-validate="'required'"
-             :class="{'is-danger': errors.has('contact-form.subject')}"> <br />
+      <div class="form-group">
+        <div class="col-3">
+          <label class="form-label" for="email">Email</label>
+        </div>
+        <div class="col-9">
+          <input type="email"
+                 id="email"
+                 class="form-input"
+                 name="email"
+                 placeholder="email"
+                 v-model="email"
+                 v-validate="'required|email'"
+                 :class="{'is-error': errors.has('contact-form.email')}">
+        </div>
+      </div>
 
-      <textarea placeholder="message"
-                name="message"
-                v-model="message"
-                v-validate="'required|max:200'"
-                :class="{'is-danger': errors.has('contact-form.message')}"></textarea> <br />
+      <div class="form-group">
+        <div class="col-3">
+          <label class="form-label" for="subject">Subject</label>
+        </div>
+        <div class="col-9">
+          <input type="text"
+                 id="subject"
+                 class="form-input"
+                 name="subject"
+                 placeholder="subject"
+                 v-model="subject"
+                 v-validate="'required'"
+                 :class="{'is-error': errors.has('contact-form.subject')}">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="col-3">
+          <label class="form-label" for="message">Message</label>
+        </div>
+        <div class="col-9">
+          <textarea placeholder="message"
+                    id="message"
+                    class="form-input"
+                    name="message"
+                    v-model="message"
+                    v-validate="'required|max:200'"
+                    :class="{'is-error': errors.has('contact-form.message')}"></textarea>
+        </div>
+      </div>
 
       <input type="submit" value="Send" class="btn btn-primary">
     </form>

5cce276b6924e162fe377ceeeba6eb84648eb73a
Ben Weber <bweber89@gmail.com>
2017-12-01 08:16:28 +0100

    Applies responsive classes to the about and home section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,47 +1,43 @@
 <template>
-  <div>
-    <h2 id="about">What We Do</h2>
-    <div>
-      <div>
-        <h4>Mobile Devices</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-          sed diam nonumy eirmod tempor invidunt ut labore et dolore
-          magna aliquyam erat, sed diam voluptua. At vero eos et
-          accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no
-        </p>
-      </div>
-      <div>
-        <h4>Desktop</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-          sed diam nonumy eirmod tempor invidunt ut labore et dolore
-          magna aliquyam erat, sed diam voluptua. At vero eos et
-          accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no
-        </p>
-      </div>
-      <div>
-        <h4>Browser</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-          sed diam nonumy eirmod tempor invidunt ut labore et dolore
-          magna aliquyam erat, sed diam voluptua. At vero eos et
-          accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no
-        </p>
-      </div>
-      <div>
-        <h4>Explorative Testing</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-          sed diam nonumy eirmod tempor invidunt ut labore et dolore
-          magna aliquyam erat, sed diam voluptua. At vero eos et
-          accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no
-        </p>
-      </div>
+  <div class="columns">
+    <div class="column col-12">
+      <h2 id="about" style="text-align: center">About Us</h2>
+    </div>
+    <div class="column col-lg-3 col-md-6 col-xs-12">
+      <h4>Dedicated Team</h4>
+      <p>
+        We are a dedicated team of Professional Software Testers, passionate about
+        In our experience, lots of software does not get tested to the level it deserves.
+      </p>
+    </div>
+
+    <div class="column col-lg-3 col-md-6 col-xs-12">
+      <h4>Passionate Bug Finders</h4>
+      <p>
+        Learn more about how we find bugs
+      </p>
+    </div>
+
+    <div class="column col-lg-3 col-md-6 col-xs-12">
+      <h4>Browsers</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore
+        magna aliquyam erat, sed diam voluptua. At vero eos et
+        accusam et justo duo dolores et ea rebum. Stet clita
+        kasd gubergren, no
+      </p>
+    </div>
+
+    <div class="column col-lg-3 col-md-6 col-xs-12">
+      <h4>Explorative Testing</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore
+        magna aliquyam erat, sed diam voluptua. At vero eos et
+        accusam et justo duo dolores et ea rebum. Stet clita
+        kasd gubergren, no
+      </p>
     </div>
   </div>
 </template>
--- src/components/HowSection.vue
+++ src/components/HowSection.vue
@@ -1,47 +1,48 @@
 <template>
-  <div>
-    <h2>How We Do</h2>
-    <div>
-      <div>
-        <h4>Experience</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-          sed diam nonumy eirmod tempor invidunt ut labore et dolore
-          magna aliquyam erat, sed diam voluptua. At vero eos et
-          accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no
-        </p>
-      </div>
-      <div>
-        <h4>With Love</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-          sed diam nonumy eirmod tempor invidunt ut labore et dolore
-          magna aliquyam erat, sed diam voluptua. At vero eos et
-          accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no
-        </p>
-      </div>
-      <div>
-        <h4>Devices</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-          sed diam nonumy eirmod tempor invidunt ut labore et dolore
-          magna aliquyam erat, sed diam voluptua. At vero eos et
-          accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no
-        </p>
-      </div>
-      <div>
-        <h4>Customer Dashboard</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-          sed diam nonumy eirmod tempor invidunt ut labore et dolore
-          magna aliquyam erat, sed diam voluptua. At vero eos et
-          accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no
-        </p>
-      </div>
+  <div class="columns">
+    <div class="column col-12">
+      <h2 style="text-align: center">How We Do It</h2>
+    </div>
+
+    <div class="column col-3 col-md-6 col-xs-12">
+      <h4>Experience</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore
+        magna aliquyam erat, sed diam voluptua. At vero eos et
+        accusam et justo duo dolores et ea rebum. Stet clita
+        kasd gubergren, no
+      </p>
+    </div>
+    <div class="column col-lg-3 col-md-6 col-xs-12">
+      <h4>With Love</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore
+        magna aliquyam erat, sed diam voluptua. At vero eos et
+        accusam et justo duo dolores et ea rebum. Stet clita
+        kasd gubergren, no
+      </p>
+    </div>
+    <div class="column col-lg-3 col-md-6 col-xs-12">
+      <h4>Devices</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore
+        magna aliquyam erat, sed diam voluptua. At vero eos et
+        accusam et justo duo dolores et ea rebum. Stet clita
+        kasd gubergren, no
+      </p>
+    </div>
+    <div class="column col-lg-3 col-md-6 col-xs-12">
+      <h4>Customer Dashboard</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore
+        magna aliquyam erat, sed diam voluptua. At vero eos et
+        accusam et justo duo dolores et ea rebum. Stet clita
+        kasd gubergren, no
+      </p>
     </div>
   </div>
 </template>

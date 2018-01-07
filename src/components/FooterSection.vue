b3bbe11f9423ef95c501e2eaaaa3ea42a9e530a4
Ben Weber <bweber89@gmail.com>
2018-01-07 16:36:43 +0100

    Styles the 'with love' heart with a scoped class

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer-section">
-    Made with <span class="text-error">♥</span> by <span class="footer-section_bugsters">Bugsters</span> <br>
+    Made with <span class="footer-section_heart">♥</span> by <span class="footer-section_bugsters">Bugsters</span> <br>
     © 2018
   </div>
 </template>
@@ -16,6 +16,9 @@ export default {
   text-align: center
   padding: 3rem 0
   color: #acb3c2
+
+  &_heart
+    color: #e85600
 
   &_bugsters
     color: #667189

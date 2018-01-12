ac7e8ec6d14007ea46ead218754641d7fc211554
Ben Weber <bweber89@gmail.com>
2018-01-12 14:31:36 +0100

    Users are shown a nice contact success or error message accordingly

--- src/components/ContactError.spec.js
+++ src/components/ContactError.spec.js
@@ -14,4 +14,13 @@ describe('ContactError Component', () => {
     const $html = wrapper.vm.$el.outerHTML
     expect($html).toMatchSnapshot()
   })
+
+  it('should emit "closeError" when clicking "try again" link', () => {
+    const wrapper = shallow(ContactError)
+    const spy = jest.spyOn(wrapper.vm, '$emit')
+    const closeLink = wrapper.find('a')
+    closeLink.trigger('click')
+    expect(spy).toHaveBeenCalledWith('closeError')
+  })
+
 })

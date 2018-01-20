ccf03fa17c1f3bb3760f21392e765e4e663a8021
Ben Weber <bweber89@gmail.com>
2018-01-20 21:51:11 +0100

    Configures jest to use a special html serializer for snapshot tests

--- jest.config.js
+++ jest.config.js
@@ -20,5 +20,9 @@ module.exports = {
   transform: {
     "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
     ".*\\.(vue)$": "<rootDir>/node_modules/jest-vue-preprocessor"
-  }
+  },
+
+  snapshotSerializers: [
+    "jest-serializer-html"
+  ]
 }
